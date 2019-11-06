72316ea03e1b086b70e9cf8264da132713f757a3
lbasurtoa <54516357+lbasurtoa@users.noreply.github.com>
2019-11-05 20:08:59 -0600

    T8

---
+++ TAREAS/8/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( argc, char *argv[]) {
+
+	int x = atof(argv[0]);
+	double y = atof(argv[1]);
+	double z;
+
+	if(x==0) {
+		z = 1.8*y + 32;
+	}
+	if(x==1) {
+		z = (5/9)*(y-32);
+	}
+
+	printf("%.2f", z);
+
+
+	return 0;
+}
