05a2545ae462f4158739c7190dae392b284d9e1d
lbasurtoa <54516357+lbasurtoa@users.noreply.github.com>
2019-11-11 17:20:09 -0600

    T7

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main( argc, char *argv[]) {
+int main( int argc, char *argv[]) {
 
-	double x = atof(argv[0]);
-	double y = atof(argv[1]);
+	double x = atof(argv[1]);
+	double y = atof(argv[2]);
 	double z = x;
 
-	for(int i=1, i<y, i++) {
+	for(int i=1; i<y; i++) {
 		x = x*z;
 	}
 
-	printf("%.2f", x);
+	printf("%.2f\n", x);
 
 
 	return 0;
