d1e3ed78c7f125953c95094b11965b3870e785ed
lbasurtoa <54516357+lbasurtoa@users.noreply.github.com>
2019-11-11 17:27:09 -0600

    T7

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -3,15 +3,15 @@
 
 int main( int argc, char *argv[]) {
 
-	double x = atof(argv[1]);
-	double y = atof(argv[2]);
-	double z = x;
+	double x = atof(argv[1]);  // El primer parametro se convierte a double
+	double y = atof(argv[2]);  // El segundo parametro se convierte a double
+	double z = x;  // El valor de 'x' se copia en la variable 'z'
 
-	for(int i=1; i<y; i++) {
+	for(int i=1; i<y; i++) {  // 'for' se encargara de multiplicar el valor de 'x' por si mismo 'y' veces
 		x = x*z;
 	}
 
-	printf("%.2f\n", x);
+	printf("%.2f\n", x);  // El valor final de 'x' que resulta en 'x^y' se imprime
 
 
 	return 0;
