6ca5494cf8953e3f6aaebef4cb72f274f6685f6d
lbasurtoa <54516357+lbasurtoa@users.noreply.github.com>
2019-11-11 18:13:31 -0600

    T2

--- TAREAS/2/main.c
+++ TAREAS/2/main.c
@@ -3,15 +3,15 @@
 
 int main (int argc, char *argv[]) {
 
-	int n; 
+	int n;  // Se declara la variable 'n' como entero
 
-	n=atoi(argv[1]);
+	n=atoi(argv[1]);  // El argumento en la posicion 1 que recibe la funcion se convierte a valor entero
 
-	if(n%2==0) {
-		printf("1\n");
+	if(n%2==0) { 
+		printf("1\n");  // Si el valor es divisible entre 2 se imprime '1'
 	}
 	else {
-		printf("0\n");
+		printf("0\n");  // Si el valor no es divisible entre 2 se imprime '2'
 	}
 
 
