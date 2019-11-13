0b37e3c25ce42d646173e3aa8e98a2f358b419a4
lbasurtoa <54516357+lbasurtoa@users.noreply.github.com>
2019-11-13 17:26:13 -0600

    T6

--- TAREAS/6/main.c
+++ TAREAS/6/main.c
@@ -5,10 +5,10 @@ int main( int argc, char *argv[]) {
 
 	int resultado;  // Resultado de la multiplicacion que se va a mostrar
 
-	for( int i=1, i<argc, i++) {  // Este 'for' recorrera la lista elemento por elemento
+	for( int i=1; i<argc; i++) {  // Este 'for' recorrera la lista elemento por elemento
 		resultado=1;
                                       // Este 'for' multiplicara a todos los elementos de la lista,
-		for( int j=1, j<argc, j++) {  // excepto excepto al de la posicion donde se encuentra
+		for( int j=1; j<argc; j++) {  // excepto excepto al de la posicion donde se encuentra
 			if(j!=i) {                          
 				resultado = resultado*atoi(argv[j]);  
 			} 
