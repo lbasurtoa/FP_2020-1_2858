db7d591d9463b637af198ed7500b27a9555c3c73
lbasurtoa <54516357+lbasurtoa@users.noreply.github.com>
2019-11-12 23:14:28 -0600

    T12

--- TAREAS/12/main.c
+++ TAREAS/12/main.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 
-int Saludos( int personas ) {
+int Saludos( int personas ) {  // Se declara la funcion que se encargara de sumar el numero de saludos
 
 	if( personas==0 ) {
 		return 0;
 	}
 
-	return Saludos(personas-1) + (personas-1);
+	return Saludos(personas-1) + (personas-1);  // La funcion sumara todos los numeros enteros desde 1 hasta el numero de personas -1
 }
 
 
-int main( int argc, char *argv[]) { 
+int main( int argc, char *argv[]) {  // En la funcion principal se llamara a la funcion 'Saludos' para hacer la suma
 
-	int personas = atoi(argv[1]);
+	int personas = atoi(argv[1]);  // El argumento ingresado en la funcion 'main' se convierte a entero para trabajar con el
 
-	printf("%i\n", Saludos(personas));
+	printf("%i\n", Saludos(personas));  // Se llama a la funcion 'Saludos' dentro de printf y se imprime el resultado
 
 
 	return 0;
