3321609e82585e1b92e1dee80756173081785170
lbasurtoa <54516357+lbasurtoa@users.noreply.github.com>
2019-11-12 23:54:02 -0600

    T9

--- TAREAS/9/main.c
+++ TAREAS/9/main.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[]) {
 
 	for(int i=0; i<nDigitos; i++) {  // Con 'for' iremos sumando cada digito en la variable 'z'
 		listaChar[i] = argv[1][i];  // Cada caracter, o digito, del numero entero ingresado se copia como 'char' en una lista 
-	    listaInt[i] = atoi(&listaChar[i]);  // Cada caracter se copia en una nueva lista pero ahora en tipo entero
-	    z = z + listaInt[i];  
+	        listaInt[i] = atoi(&listaChar[i]);  // Cada caracter se copia en una nueva lista pero ahora en tipo entero
+	        z = z + listaInt[i];  
 	}
 
 	printf("%i\n", z);  // Se imprime el resultado de la suma
